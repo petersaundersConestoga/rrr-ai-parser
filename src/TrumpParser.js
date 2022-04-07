bc7f2b814d07f9f92cb348d985bd2a1669763cb7
petersaundersConestoga <78880664+petersaundersConestoga@users.noreply.github.com>
2022-04-06 22:01:48 -0400

    modifying to match yelp format for text, body

--- src/TrumpParser.js
+++ src/TrumpParser.js
@@ -16,9 +16,62 @@ class TrumpParser extends React.Component {
         let myarray = JSON.parse(JSON.stringify(myJson));
         console.log(myarray);
         this.setState({myout: myarray})
-        this.parseTweet(myarray);
+        //this.parseTweet(myarray);
+        this.removeKey(myarray);
+    }
+
+    removeKey(myarray) {
+        // remove http/s as well
+        let patternHttps = /https/i;
+        let patternHttp = /http/i;
+        let patternAt = /@/i;
+        let patternHash = /#/i;
+
+        let i = 0;
+
+        //let trumpTweets = JSON.parse(JSON.stringify(myarray));
+
+        let trumpTweets = [];
+        let mytweet = "";
+        myarray.slice(0, 10).map(item => {
+            mytweet = "";
+            mytweet = JSON.stringify(item, (key, value) => {
+                /*
+                "id": 1234653427789070300,
+        "text": "I was thrilled to be back in the Great city of Charlotte, North Carolina with thousands of hardworking American Patriots who love our Country, cherish our values, respect our laws, and always put AMERICA FIRST! Thank you for a wonderful evening!! #KAG2020 https://t.co/dNJZfRsl9y",
+        "isRetweet": "f",
+        "isDeleted": "f",
+        "device": "Twitter for iPhone",
+        "favorites": 73748,
+        "retweets": 17404,
+        "date": "2020-03-03 01:34:50",
+        "isFlagged": "f"
+        */
+                if (key === "id" ||
+                    key === "isRetweet" ||
+                    key === "isDeleted" ||
+                    key === "device" ||
+                    key === "favorites" ||
+                    key === "retweets" ||
+                    key === "date" ||
+                    key === "isFlagged") { 
+                    return undefined;
+                }
+                console.log(value);
+                return value;
+            });
+
+            i++;
+            console.log(mytweet);
+            trumpTweets.push(mytweet);
+        })
+
+        console.log(trumpTweets);
+
+        this.setState({machineOut: trumpTweets, tweetCount: i});
     }
     
+    /*
     parseTweet(arr) {
         let prepMachineLearning = "";
         let tag = this.state.tag;
@@ -56,6 +109,7 @@ class TrumpParser extends React.Component {
 
         this.setState({machineOut: prepMachineLearning, tweetCount: i});
     }
+    */
 
     render() {
         return (
